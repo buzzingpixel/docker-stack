ab23bfe4766bde4b4b97f22402db0b3ebefe2aab
TJ Draper <tj@buzzingpixel.com>
2018-05-16 00:31:49 -0500

    Add PHP 7.1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
 
 phpinfo();
+
+// var_dump(function_exists('gd_info'));
+
+// var_dump(class_exists('Imagick'));
+
+// $pdo = new PDO('mysql:host=db;port=3306;charset=utf8;dbname=site', 'site', 'secret');
+
+// $createTable = $pdo->exec('CREATE TABLE IF NOT EXISTS testTable(ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50))');
+
+// $query = $pdo->query('SELECT * FROM testTable')->fetch();
+
+// var_dump($createTable, $query);
+
+// $link = mysqli_connect('db', 'site', 'secret', 'site', '3306');
+
+// $link->query('CREATE TABLE IF NOT EXISTS testTable(ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50))');
+
+// $link->query("INSERT INTO testTable (name) VALUES ('Johnny')");
+
+// var_dump($link->query("SELECT * FROM testTable"));

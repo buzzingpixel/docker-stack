ac5435add02cfa65210160e4461ebe0b4fb3b3ee
TJ Draper <tj@buzzingpixel.com>
2018-05-23 12:43:08 -0500

    Add image testing

--- public/index.php
+++ public/index.php
@@ -2,6 +2,26 @@
 
 phpinfo();
 
+// ImageMagick Test
+// @unlink(__DIR__ . '/cache/national-park-wikimedia-commons-imagemagick-400x400.jpg');
+// $image = new \Imagick(__DIR__ . '/testImages/national-park-wikimedia-commons.jpg');
+// $image->resizeImage(400, 400, Imagick::COLOR_ALPHA, 1);
+// $image->writeImage(__DIR__ . '/cache/national-park-wikimedia-commons-imagemagick-400x400.jpg');
+// die;
+
+// GD test
+// $filename = __DIR__ . '/testImages/national-park-wikimedia-commons.jpg';
+// $newFilename = __DIR__ . '/cache/national-park-wikimedia-commons-gd-400x400.jpg';
+// @unlink($newFilename);
+// $source = imagecreatefromjpeg($filename);
+// list($width, $height) = getimagesize($filename);
+// $newwidth = 400;
+// $newheight = 400;
+// $destination = imagecreatetruecolor(400, 400);
+// imagecopyresampled($destination, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+// imagejpeg($destination, $newFilename, 100);
+// die;
+
 // var_dump(function_exists('gd_info'));
 
 // var_dump(class_exists('Imagick'));
